e89feac2dedf0e3fec124227ec3707252d5591fb
austinrafter <56013277+austinrafter@users.noreply.github.com>
2022-03-27 12:24:18 -0700

    Initial commit

---
+++ ExerciseAndMeditationMicroService/ExerciseAndMeditation/src/main/java/com/example/exerciseandmeditation/ExerciseAndMeditationApplication.java
@@ -0,0 +1,13 @@
+package com.example.exerciseandmeditation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseAndMeditationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseAndMeditationApplication.class, args);
+    }
+
+}
