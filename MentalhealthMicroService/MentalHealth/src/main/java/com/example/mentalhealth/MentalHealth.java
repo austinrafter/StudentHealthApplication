0bbc344aeda20586662148a5563acd129bb3f2a8
sophiayuanproj <sophia38yuan@gmail.com>
2022-03-27 13:09:39 -0700

    Sophia- Initial commit

---
+++ MentalhealthMicroService/MentalHealth/src/main/java/com/example/mentalhealth/MentalHealth.java
@@ -0,0 +1,13 @@
+package com.example.exerciseandmeditation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseAndMeditationApplication.class, args);
+    }
+
+}
