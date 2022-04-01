89150e44533cfbcb2b7b0934566ece57273e5981
Lee71559 <lee71559@gmail.com>
2022-03-31 20:07:20 -0700

    Shan initital commit

---
+++ StudyHabitsMicroService/src/main/java/com/example/studyhabitsmicroservice/StudyHabitsMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyhabitsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHabitsMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyHabitsMicroServiceApplication.class, args);
+    }
+
+}
