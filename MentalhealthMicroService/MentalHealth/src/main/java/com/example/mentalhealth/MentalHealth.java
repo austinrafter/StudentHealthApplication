4d3c8bd71f63fccb4495ab179b2cb874e2d86c68
sophiayuanproj <sophia38yuan@gmail.com>
2022-04-15 04:18:31 -0700

    Sophia- added classes

--- MentalhealthMicroService/MentalHealth/src/main/java/com/example/mentalhealth/MentalHealth.java
+++ MentalhealthMicroService/MentalHealth/src/main/java/com/example/mentalhealth/MentalHealth.java
@@ -1,4 +1,4 @@
-package com.example.exerciseandmeditation;
+package com.example.mentalhealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
