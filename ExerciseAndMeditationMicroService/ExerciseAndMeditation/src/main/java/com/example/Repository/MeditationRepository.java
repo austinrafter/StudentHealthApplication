8ae8a439ded452640d5786c49e8d31994b601f3c
austinrafter <56013277+austinrafter@users.noreply.github.com>
2022-04-18 19:36:57 -0700

    Fixed repositories. JPA repos may be necessary for my microservices

--- ExerciseAndMeditationMicroService/ExerciseAndMeditation/src/main/java/com/example/Repository/MeditationRepository.java
+++ ExerciseAndMeditationMicroService/ExerciseAndMeditation/src/main/java/com/example/Repository/MeditationRepository.java
@@ -2,7 +2,9 @@ package com.example.Repository;
 import com.example.Model.Meditation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MeditationRepository extends JpaRepository{
-    Meditation findByMeditationName(String meditationName);
-    Meditation findByMeditationType(String meditationType);
+    List<Meditation> findByMeditationName(String meditationName);
+    List<Meditation> findByMeditationType(String meditationType);
 }
