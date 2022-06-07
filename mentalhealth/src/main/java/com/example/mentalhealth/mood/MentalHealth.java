dfec48ebb45637546a190f3ac6cf7cb6bd5cdff5
sophiayuanproj <sophia38yuan@gmail.com>
2022-06-06 19:55:47 -0700

    Created new file and worked on mood for springboot

---
+++ mentalhealth/src/main/java/com/example/mentalhealth/mood/MentalHealth.java
@@ -0,0 +1,22 @@
+package com.example.mentalhealth.mood;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+@Table(name = "MENTAL_HEALTH")
+public class MentalHealth {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String mood;
+    private String month;
+    private String year;
+    private String day;
+}
