fe728e962613496000f37c924e771405bd70130a
Lee71559 <lee71559@gmail.com>
2022-09-09 13:17:25 -0700

    fix studyhabits backend

--- StudyHabitsMicroService/src/main/java/com/example/studyhabitsmicroservice/model/Activity.java
+++ StudyHabitsMicroService/src/main/java/com/example/studyhabitsmicroservice/model/Activity.java
@@ -1,7 +1,5 @@
 package com.example.studyhabitsmicroservice.model;
 
-import lombok.Getter;
-import lombok.Setter;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
--- StudyHabitsMicroService/src/main/java/com/example/studyhabitsmicroservice/model/StudyBlock.java
+++ StudyHabitsMicroService/src/main/java/com/example/studyhabitsmicroservice/model/StudyBlock.java
@@ -1,8 +1,6 @@
 package com.example.studyhabitsmicroservice.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
