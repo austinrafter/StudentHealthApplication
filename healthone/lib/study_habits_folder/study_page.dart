d09b80e481a1f76e497babaa945cd8471456356a
Lee71559 <lee71559@gmail.com>
2022-09-20 14:20:17 -0700

    Update study habits main page

--- healthone/lib/study_habits_folder/study_page.dart
+++ healthone/lib/study_habits_folder/study_page.dart
@@ -6,36 +6,31 @@ import 'study_timer_page.dart';
 import 'classes_page.dart';
 import 'study_page_data.dart';
 
-class StudyPage extends StatelessWidget{
+class StudyPage extends StatelessWidget {
   //const StudyPage({Key? key}) : super(key: key);
   var studyPageData = StudyPageData.getData;
 
-  final pages = [
-   StudyTimerPage(),
-   ClassesPage()
-  ];
+  final pages = [StudyTimerPage(), ClassesPage()];
 
-  final images =[
-    "https://media.giphy.com/media/TF6FLfa5NryGdEJcfY/giphy.gif",
-    "https://media.giphy.com/media/iLBQAlaft9NU4/giphy.gif",
-    "https://media.giphy.com/media/kIRicSBQwa23pYExQT/giphy.gif",
-    "https://media.giphy.com/media/t9811uoqdhx9pQZx3z/giphy.gif",
+  final images = [
+    "https://media.giphy.com/media/yovOUEWBV2R46yrQ0B/giphy.gif",
+    "https://media.giphy.com/media/2xnO6tTIYYFE2j3IqQ/giphy.gif",
   ];
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[500],
+      backgroundColor: Colors.blue[900],
       appBar: AppBar(
         title: const Text("Study Habits"),
         centerTitle: true,
-      ),  
+      ),
 
       body: Container(
-      padding: const EdgeInsets.fromLTRB(10,10,10,0),
-      height: 700,
-      width: double.maxFinite,
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        height: 700,
+        width: double.maxFinite,
 
         child: Column(
           mainAxisSize: MainAxisSize.max,
@@ -44,71 +39,69 @@ class StudyPage extends StatelessWidget{
             Expanded(
               child: ListView.builder(
                 itemCount: studyPageData.length,
-                itemBuilder: (context, index){
+                itemBuilder: (context, index) {
                   return Container(
                     padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
                     height: 220,
                     width: double.maxFinite,
-                  child: Card(
-                    elevation: 5,
-                    color: Colors.blue[900],
+                    child: Card(
+                      elevation: 5,
+                      color: Colors.blue[500],
                       //child: Padding(
-                        //padding: EdgeInsets.all(0),
-                    //child: Column(
-                        //children: [
-                          child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) =>  pages[index]),
-                            );
-                          },
-                          child:Card(
-                            color: Colors.blue[700],
-                            child: Column(
-                              children: <Widget>[
-                                //Center(
-                                  //child:
-                                  Container(
-                                    alignment: Alignment.center,
-                                    child:  PhysicalItemPageName(studyPageData[index]),
-                                  ),//Container
-                                //),//Center
-                                Image.network(
-                                  '${images[index]}',
-                                  width: double.infinity,
-                                  height: 140,
-                                  //fit: Boxfit.cover
-                                )//image.network
-                              ],//children
-                            ),//Stack
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Colors.blue.shade900,
-                              ),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),//RoundedRectandleBorder
-                            elevation: 5,
-                            margin: const EdgeInsets.all(4.0),
-                          ),//child: Card
-                        ),//child: GestureDetector
-                    //],
-                     // ),// child: Padding
-                  ),//child: Card
-                  );//Container
-                },//itemBuilder
-              ),//child: ListView.builder
-            ),//Expanded
-
-            ],//children: <Widget>
-      ),//child: Column
-
-      ),//body: Container
-
-    );//Scaffold
-  }//Widget build
-}//class
-
+                      //padding: EdgeInsets.all(0),
+                      //child: Column(
+                      //children: [
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => pages[index]),
+                          );
+                        },
+                        child: Card(
+                          color: Colors.blue[700],
+                          child: Column(
+                            children: <Widget>[
+                              //Center(
+                              //child:
+                              Container(
+                                alignment: Alignment.center,
+                                child:
+                                    PhysicalItemPageName(studyPageData[index]),
+                              ), //Container
+                              //),//Center
+                              Image.network(
+                                '${images[index]}',
+                                width: double.infinity,
+                                height: 140,
+                                //fit: Boxfit.cover
+                              ) //image.network
+                            ], //children
+                          ), //Stack
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.blue.shade900,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ), //RoundedRectandleBorder
+                          elevation: 5,
+                          margin: const EdgeInsets.all(4.0),
+                        ), //child: Card
+                      ), //child: GestureDetector
+                      //],
+                      // ),// child: Padding
+                    ), //child: Card
+                  ); //Container
+                }, //itemBuilder
+              ), //child: ListView.builder
+            ), //Expanded
+          ], //children: <Widget>
+        ), //child: Column
+      ), //body: Container
+    ); //Scaffold
+  } //Widget build
+} //class
 
 Widget PhysicalItemPageName(data) {
   return Align(
