01d763bd810873cfa729e4194e370efd08e69255
Lee71559 <lee71559@gmail.com>
2022-09-27 22:22:36 -0700

    Finished 'classes' page, user can add, edit, delete

--- healthone/lib/study_habits_folder/classes_folder/classes_info.dart
+++ healthone/lib/study_habits_folder/classes_folder/classes_info.dart
@@ -17,6 +17,31 @@ class ClassesInfo extends StatefulWidget {
 
 class _ClassesState extends State<ClassesInfo> {
   List<StudyClass>? classes;
+  List<String> semesters = <String>['Spring', 'Summer', 'Fall', 'Winter'];
+  List<String> years = <String>[
+    '2010',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+    '2020',
+    '2021',
+    '2022',
+    '2023'
+  ];
+  List<String> grades = <String>['A', 'B', 'C', 'D', 'E', 'F'];
+
+  var classCode;
+  var className;
+  var classPoint;
+  var _classSem = '';
+  var _classYear = '';
+  var _classGrade = '';
 
   getClasses() async {
     classes = await ClassesDBService.getClasses();
@@ -49,10 +74,9 @@ class _ClassesState extends State<ClassesInfo> {
       body: Column(
         children: [
           buildClassesTiles(),
-          //buildAddClassButton(),
         ],
       ),
-      floatingActionButton: buildAddClassButton(false),
+      floatingActionButton: buildAddClassButton(),
     );
   }
 
@@ -93,10 +117,27 @@ class _ClassesState extends State<ClassesInfo> {
                       ),
                     ),
                     subtitle: Text(
-                      "${curClassData.semester}\n${curClassData.point} ${curClassData.grade}",
+                      "${curClassData.semester}\n${curClassData.point} - ${curClassData.grade}",
                       style: const TextStyle(
                         color: Colors.white,
                       ),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              showClassForm(true, curClassData);
+                            },
+                            icon: const Icon(Icons.edit)),
+                        IconButton(
+                            onPressed: () {
+                              print("==========Delete Class==========");
+                              ClassData().deleteClass(curClassData);
+                              print("==========Finish deleting==========");
+                            },
+                            icon: const Icon(Icons.delete)),
+                      ],
                     ),
                   ),
                 );
@@ -106,117 +147,243 @@ class _ClassesState extends State<ClassesInfo> {
   }
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  Widget buildAddClassButton(bool update) {
-    String titleText = "Add Class";
-    if (update) titleText = "Update Class";
+  Widget buildAddClassButton() {
+    _classSem = semesters.first;
+    _classYear = years.first;
+    _classGrade = grades.first;
 
     return IconButton(
-      icon: const Icon(Icons.add_circle),
-      color: Colors.white,
-      iconSize: 50.0,
-      onPressed: () => showDialog(
+        icon: const Icon(Icons.add_circle),
+        color: Colors.white,
+        iconSize: 50.0,
+        onPressed: () {
+          showClassForm(
+              false,
+              StudyClass(
+                  id: 1,
+                  code: 'code',
+                  name: 'name',
+                  semester: 'semester',
+                  point: 0.0,
+                  grade: 'grade'));
+        });
+  }
+
+  void showClassForm(bool update, StudyClass curClassData) {
+    var titleText = 'Add Class';
+    if (update) {
+      titleText = 'Update Class';
+      classCode = Text(curClassData.code);
+      className = Text(curClassData.name);
+      classPoint = Text(curClassData.point.toString());
+      _classGrade = Text(curClassData.grade) as String;
+      _classYear = curClassData.semester.substring(2);
+      switch (curClassData.semester.substring(0, 2)) {
+        case 'SP':
+          _classSem = semesters.first;
+          break;
+        case 'SU':
+          _classSem = semesters.elementAt(1);
+          break;
+        case 'FA':
+          _classSem = semesters.elementAt(2);
+          break;
+        case 'WI':
+          _classSem = semesters.last;
+          break;
+        default:
+          _classSem = semesters.first;
+          break;
+      }
+    }
+
+    showDialog(
         context: context,
-        builder: (context) => AlertDialog(
-          title: Text(titleText),
-          content: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Row(
-                    children: <Widget>[
-                      const Text("Class code: "),
-                      Expanded(
-                        child: TextFormField(
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter class code';
-                            }
-                            return null;
-                          },
+        builder: (context) => Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: SingleChildScrollView(
+                  child: AlertDialog(
+                title: Text(titleText),
+                content: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            const Text("Class code: "),
+                            Expanded(
+                              child: TextFormField(
+                                controller: classCode,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter class code';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const Text("Class name: "),
-                      Expanded(
-                        child: TextFormField(
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter class name';
-                            }
-                            return null;
-                          },
+                        Row(
+                          children: <Widget>[
+                            const Text("Class name: "),
+                            Expanded(
+                              child: TextFormField(
+                                controller: className,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter class name';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text("Semester: "),
-                      Expanded(
-                        child: TextFormField(
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter semester';
-                            }
-                            return null;
-                          },
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const Text("Semester: "),
+                            Expanded(
+                                child: DropdownButtonFormField(
+                              value: _classSem,
+                              icon: const Icon(Icons.expand_more),
+                              onSaved: (String? value) {
+                                setState(() {
+                                  _classSem = value!;
+                                });
+                              },
+                              items: semesters.map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _classSem = value!;
+                                });
+                              },
+                            )),
+                            Expanded(
+                                child: DropdownButtonFormField(
+                              value: _classYear,
+                              icon: const Icon(Icons.expand_more),
+                              onSaved: (String? value) {
+                                setState(() {
+                                  _classYear = value!;
+                                });
+                              },
+                              items: years.map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _classYear = value!;
+                                });
+                              },
+                            )),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text("Point: "),
-                      Expanded(
-                        child: TextFormField(
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter point';
-                            } else if (double.tryParse(value) == null) {
-                              return 'Invalid point';
-                            }
-                            return null;
-                          },
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const Text("Point: "),
+                            Expanded(
+                              child: TextFormField(
+                                controller: classPoint,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter point';
+                                  } else if (double.tryParse(value) == null) {
+                                    return 'Invalid point';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text("Grade: "),
-                      Expanded(
-                        child: TextFormField(
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter grade';
-                            }
-                            return null;
-                          },
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const Text("Grade: "),
+                            Expanded(
+                                child: DropdownButtonFormField(
+                              value: _classGrade,
+                              icon: const Icon(Icons.expand_more),
+                              onSaved: (String? value) {
+                                setState(() {
+                                  _classGrade = value!;
+                                });
+                              },
+                              items: grades.map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _classGrade = value!;
+                                });
+                              },
+                            )),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          // Process data.
-                        }
-                      },
-                      child: const Text("Submit"),
-                    ),
-                  ),
-                ],
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                print("=========Add Class==========");
+                                switch (_classSem) {
+                                  case 'Spring':
+                                    _classSem = 'SP$_classYear';
+                                    break;
+                                  case 'Summer':
+                                    _classSem = 'SU$_classYear';
+                                    break;
+                                  case 'Fall':
+                                    _classSem = 'FA$_classYear';
+                                    break;
+                                  case 'Winter':
+                                    _classSem = 'WI$_classYear';
+                                    break;
+                                  default:
+                                    _classSem = '';
+                                }
+                                ClassData().addClass(
+                                    classCode.toString(),
+                                    className.toString(),
+                                    _classSem,
+                                    double.tryParse(classPoint.toString())!,
+                                    _classGrade);
+                                print("=========Finish Adding==========");
+                              }
+                            },
+                            child: const Text("Submit"),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true)
+                                  .pop('dialog');
+                            },
+                            child: const Text("Cancel"),
+                          ),
+                        ),
+                      ],
+                    )),
               )),
-        ),
-      ),
-    );
+            ));
   }
 }
